54fc6ac9a25ce2e6f9ff29e5a84e9d7e64fdc542
harshal-08 <daine1108harshal@gmail.com>
2021-04-03 14:07:34 +0530

    added prediction website and gitignore

--- prediction_website/prediction_website/settings.py
+++ prediction_website/prediction_website/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@!7+m=e4r=)$4gm1^x3kas&bla_oafh4b835(=dm6!ej1g4r#6'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
